1aa8fc14b6cbb4b9d631676f2aed3b6a8ab787c4
Abby Howell <abby.g.howell@gmail.com>
2013-10-10 11:15:55 -0400

    post, edit, delete all working

--- main.rb
+++ main.rb
@@ -16,21 +16,35 @@ get '/posts' do
   erb :post_index
 end
 
+get '/posts/new' do
+  erb :post_new
+end
+
+post '/posts/create' do
+	title = params[:title].gsub(/['"]/, "''")
+	body = params[:body].gsub(/['"]/, "''")
+	query("INSERT INTO blog (title, body) VALUES ('#{title}', '#{body}')")
+	redirect '/posts'
+end
+
 get '/posts/:id' do
   @post = query("SELECT * FROM blog where id = #{params[:id]} LIMIT 1").first
   erb :post_show
 end
 
-delete '/posts/:id/delete' do
+get '/posts/:id/edit' do
+  @post = query("SELECT * FROM blog where id = #{params[:id]} LIMIT 1").first
+  erb :post_edit
 end
 
-get '/posts/new' do
-
-  erb :post_new
+post '/posts/:id/update' do
+  query("UPDATE blog SET title='#{params[:title]}', body='#{params[:body]}' WHERE id=#{params[:id]}")
+  redirect '/posts'
 end
 
 
-post '/posts/create' do
-  
-
+post '/posts/:id/delete' do
+  query("DELETE FROM blog WHERE id=#{params[:id]}")
+  redirect '/posts'
 end
+
