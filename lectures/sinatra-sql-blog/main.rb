3e2e8e81ee15033f6655940577a16e2c08caa6ae
David Fisher <tibbon@gmail.com>
2013-10-10 09:40:51 -0400

    started creating CRUD actions

---
+++ lectures/sinatra-sql-blog/main.rb
@@ -0,0 +1,32 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'pg'
+
+configure do
+	CONN = PG.connect(dbname: 'ga-class' ,host: 'localhost')
+end
+
+def query(sql)
+	CONN.exec(sql)
+end
+
+get '/posts' do
+	@posts = query("SELECT * FROM blog")
+	erb :post_index
+end
+
+get '/posts/:id' do
+	post_id = params[:id]
+	@post = query("SELECT * FROM blog WHERE id = #{post_id} LIMIT 1")
+	erb :post_show
+end
+
+# This will be for showing the form to create new posts
+get '/posts/new' do
+	erb :post_new
+end
+
+post '/posts/create' do
+	# I'll need something here to create a post
+end
